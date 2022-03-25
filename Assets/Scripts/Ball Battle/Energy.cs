756b447a77ff8a40e36909aa0fc5e80a28b2e8a0
peculiarnewbie <curiousarmand@gmail.com>
2022-03-25 07:01:32 +0700

    Scene

--- Assets/Scripts/Ball Battle/Energy.cs
+++ Assets/Scripts/Ball Battle/Energy.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,26 +7,39 @@ public class Energy : MonoBehaviour
 {
     [SerializeField] Image energyBar;
     [SerializeField] Image activatedBar;
+    float actualEnergy;
 
     int filledEnergy;
-    int activatedEnergy;
+    public int activatedEnergy;
 
     // Start is called before the first frame update
     void Start()
     {
-        energyBar.fillAmount = 0.0f;
-        activatedBar.fillAmount = 0.0f;
+        actualEnergy = energyBar.fillAmount = activatedBar.fillAmount = 0.0f;
         filledEnergy = activatedEnergy = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        energyBar.fillAmount += 0.5f*Time.deltaTime/6f;
+        if(actualEnergy > 6f) return;
+        actualEnergy += 0.5f*Time.deltaTime/6f;
+        UpdateFillAmount();
+    }
+
+    private void UpdateFillAmount(){
+        energyBar.fillAmount = actualEnergy;
+
         filledEnergy = (int) (energyBar.fillAmount * 6);
-        if(filledEnergy > activatedEnergy){
+        
+        if(filledEnergy != activatedEnergy){
             activatedEnergy = filledEnergy;
             activatedBar.fillAmount = (float) activatedEnergy / 6f;
         }
+    }
+
+    public void UseEnergy(int cost){
+        if(actualEnergy >= 6f) actualEnergy = 4f; //to keep value from exceeding 6
+        else actualEnergy -= 2f;
     }
 }
